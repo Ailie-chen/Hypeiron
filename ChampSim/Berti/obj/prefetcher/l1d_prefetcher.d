41cb65281ce3d5282640f27e8bb0565cfe1da374
chenwei <1668956356@qq.com>
2023-06-28 03:53:07 +0000

    size_4x运行出错，vberti优先发送预取

---
+++ ChampSim/Berti/obj/prefetcher/l1d_prefetcher.d
@@ -0,0 +1,20 @@
+obj/prefetcher/l1d_prefetcher.o: prefetcher/l1d_prefetcher.cc \
+ prefetcher/vbertim.h prefetcher/vberti_size.h inc/cache.h \
+ inc/memory_class.h inc/champsim.h inc/block.h inc/instruction.h \
+ inc/set.h
+
+prefetcher/vbertim.h:
+
+prefetcher/vberti_size.h:
+
+inc/cache.h:
+
+inc/memory_class.h:
+
+inc/champsim.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/set.h:
