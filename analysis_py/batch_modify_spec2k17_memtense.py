7b64b2b6bf97482eebb733fc66fb2c7e1df778ed
chenwei <1668956356@qq.com>
2023-07-19 02:28:11 +0000

    现在可以批量修改日期调用bingo_evaluate,又不需要大改脚本，一种中和

--- analysis_py/batch_modify_spec2k17_memtense.py
+++ analysis_py/batch_modify_spec2k17_memtense.py
@@ -3,9 +3,9 @@ import os
 from bingo_evaluate import bingo_evaluate
 
 # 定义要更改的日期列表
-# dates_modify=['0703','0704','0705','0706','0708','0709','0710',
-#               '0711','0712','0713','0714','0715','0716','0717','0718']
-dates_modify=['0703']
+dates_modify=['0703','0704','0705','0706','0708','0709','0710',
+              '0711','0712','0713','0714','0715','0716','0717','0718']
+#dates_modify=['0703']
 # 遍历日期列表
 for date in dates_modify:
     # 打开json文件
@@ -17,7 +17,7 @@ for date in dates_modify:
     data['results_dir'] = ["outputsum/output0702/spec2k17","outputsum/output"+date+"/spec2k17"]
 
     # 保存修改后的json文件
-    with open('1core_spec2k17_compare_mem_tense.json', 'w') as json_file:
+    with open('analysis_py/1core_spec2k17_compare_mem_tense.json', 'w') as json_file:
         json.dump(data, json_file, indent=4)
 
     # 运行bingo_evaluate()函数
